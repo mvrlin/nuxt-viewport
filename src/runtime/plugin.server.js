cfa8719906d4416dce92ff4ab552e948c88c090b
mvrlin <mvrlin@pm.me>
2021-06-17 13:04:17 +0300

    fix(runtime): detect breakpoint only with headers

--- src/runtime/plugin.server.js
+++ src/runtime/plugin.server.js
@@ -8,17 +8,13 @@ export default async function (ctx, inject) {
   const options = <%= serialize(options) %>
   const { req = {} } = ctx.ssrContext
 
-  let cookieViewport = ''
-  let requestCookies = {}
-  let userAgent = ''
+  let breakpoint = options.fallbackBreakpoint
 
   if (req.headers) {
-    requestCookies = cookie.parse(req.headers.cookie || '')
-    cookieViewport = requestCookies[options.cookieName]
-    userAgent = req.headers['user-agent']
+    const requestCookies = cookie.parse(req.headers.cookie || '')
+    breakpoint = await detectBreakpoint.call(options, requestCookies[options.cookieName], req.headers['user-agent'])
   }
 
-  const breakpoint = await detectBreakpoint.call(options, cookieViewport, userAgent)
   const viewport = useViewport(options, breakpoint)
 
   ctx.beforeNuxtRender(({ nuxtState }) => {
