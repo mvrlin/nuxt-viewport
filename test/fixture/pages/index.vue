a9bfe147335d1eed0f2627b8e6580a4797101180
mvrlin <mvrlin@pm.me>
2021-12-15 10:47:01 +0300

    fix(lint): fix eslint errors

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -5,14 +5,16 @@
     </p>
 
     <template v-for="breakpoint in ['mobile', 'tablet', 'desktop']">
-      <hr>
+      <div :key="breakpoint">
+        <hr>
 
-      <h4>{{ breakpoint }}</h4>
+        <h4>{{ breakpoint }}</h4>
 
-      <p><code>match</code> --> <b>{{ $viewport.match(breakpoint) }}</b></p>
-      <p><code>isLessThan</code> --> <b>{{ $viewport.isLessThan(breakpoint) }}</b></p>
-      <p><code>isGreaterThan</code> --> <b>{{ $viewport.isGreaterThan(breakpoint) }}</b></p>
-      <p><code>isGreaterOrEquals</code> --> <b>{{ $viewport.isGreaterOrEquals(breakpoint) }}</b></p>
+        <p><code>match</code> → <b>{{ $viewport.match(breakpoint) }}</b></p>
+        <p><code>isLessThan</code> → <b>{{ $viewport.isLessThan(breakpoint) }}</b></p>
+        <p><code>isGreaterThan</code> → <b>{{ $viewport.isGreaterThan(breakpoint) }}</b></p>
+        <p><code>isGreaterOrEquals</code> → <b>{{ $viewport.isGreaterOrEquals(breakpoint) }}</b></p>
+      </div>
     </template>
 
   </section>
@@ -29,50 +31,50 @@ export default defineComponent({
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 
-  html {
-    font-family: sans-serif;
-    font-size: 120%;
-  }
+html {
+  font-family: sans-serif;
+  font-size: 120%;
+}
 
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-  }
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
 
-  section {
-    max-width: calc(100vw - 1rem);
-    background: #eee;
-    padding: 2rem;
-    border-radius: 1rem;
-  }
+section {
+  max-width: calc(100vw - 1rem);
+  background: #eee;
+  padding: 2rem;
+  border-radius: 1rem;
+}
 
-  hr {
-    display: block;
-    border: none;
-    border-top: .1rem dashed #ccc;
-    margin: 1rem auto;
-  }
+hr {
+  display: block;
+  border: none;
+  border-top: 0.1rem dashed #ccc;
+  margin: 1rem auto;
+}
 
-  p {
-    margin: .5rem 0;
-  }
+p {
+  margin: 0.5rem 0;
+}
 
-  h4 {
-    text-decoration: underline;
-  }
+h4 {
+  text-decoration: underline;
+}
 
-  code {
-    display: inline-block;
-    padding: .25em .35rem;
-    background: #ccc;
-    border-radius: .25rem;
-  }
+code {
+  display: inline-block;
+  padding: 0.25em 0.35rem;
+  background: #ccc;
+  border-radius: 0.25rem;
+}
 </style>
