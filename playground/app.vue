ff0cad3dada7f628300c12feed7067ae4116e430
Valeriy Sinevich <zohan33333@gmail.com>
2023-10-28 02:17:09 +0300

    46-hydration-mismatch-playground

---
+++ playground/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useNuxtApp } from '#imports'
+
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook('app:suspense:resolve', () => {
+  console.log('app:suspense:resolve', performance.now())
+})
+
+nuxtApp.hook('app:mounted', () => {
+  console.log('app:mounted', performance.now())
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout :name='$viewport.breakpoint.value.includes("mobile") ? "mobile" : "desktop"'>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
